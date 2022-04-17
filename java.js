df2ade5d1fe6fda73db5a62592a2efa408cc49a0
gabriel <gmoraes.mat@gmail.com>
2022-04-17 01:06:54 -0300

    Terceira versão, usuario online

---
+++ java.js
@@ -0,0 +1,54 @@
+let requisicao = "";
+let nome = "";
+
+
+
+function inicio(){
+    //alert("inicio funcioanndo");
+    
+    let nomeusuario = prompt("Qual o seu nome ?") ;
+        
+    nome = {name: nomeusuario};    
+    requisicao = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', nome);    
+    requisicao.then(tratarSucesso);
+    requisicao.catch(tratarErro);   
+}
+inicio();
+
+function tratarErro(){ 
+  
+    console.log("deu ruim");
+    alert("Por favor, entre com outro nome de usuario !");
+    inicio();   
+ }
+
+ function tratarSucesso(){
+    console.log("deu bom"); 
+    alert("Bom bapo ;)");
+    confirmacaoStatus();      
+ }
+ function confirmacaoStatus(){
+    requisicao = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', nome); }
+ const meuInterval = setInterval(confirmacaoStatus, 4500);
+
+    
+
+function carregarpagina(){
+
+}
+
+//carregarpagina();  //carrega msg do servidor
+
+function enviar(){
+
+}
+
+function saida(sair){
+
+
+}
+
+
+
+
+    
