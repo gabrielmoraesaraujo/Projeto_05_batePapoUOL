7051f139726e1fcc2b025ddbbcd4ec7a884a2824
gabriel <gmoraes.mat@gmail.com>
2022-04-18 02:22:49 -0300

    Quarta versão, recebimento de msg

--- java.js
+++ java.js
@@ -1,5 +1,7 @@
 let requisicao = "";
 let nome = "";
+let teste =[];
+let variavellocal = [];
 
 
 
@@ -23,21 +25,67 @@ function tratarErro(){
  }
 
  function tratarSucesso(){
-    console.log("deu bom"); 
-    alert("Bom bapo ;)");
-    confirmacaoStatus();      
+    //console.log("deu bom"); 
+   // alert("Bom bapo ;)");
+    variavellocal = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages"); 
+    variavellocal.then(carregarpagina);     
  }
- function confirmacaoStatus(){
-    requisicao = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', nome); }
- const meuInterval = setInterval(confirmacaoStatus, 5000);
 
+ function confirmacaoStatus(){
+    requisicao = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', nome); 
+    //console.log(teste.length);
+    //console.log(variavellocal.length);
+}
+ const meuInterval = setInterval(confirmacaoStatus, 5000);
+ const carregaInterval = setInterval(tratarSucesso, 3000);
     
 
-function carregarpagina(){
+function carregarpagina(mensagens){
+    mensagens = mensagens.data;
+    let chat = document.querySelector(".meio");
+    //chat.scrollIntoView(true);
+    chat.innerHTML = "";
+    
 
-}
+    for(i=0; i<mensagens.length; i++){
 
-//carregarpagina();  //carrega msg do servidor
+        if(mensagens[i].type==="status"){
+            chat.innerHTML += `
+        <li class="li.entrada">
+        <div class="caixamensagem">
+        <span> (${mensagens[i].time}) ${mensagens[i].from} ${mensagens[i].text} </span> 
+        </div>
+        `
+        chat.scrollIntoView(false);
+
+        }
+        if(mensagens[i].type==="message"){
+            chat.innerHTML += `
+        <li class="status">
+        <div class="caixamensagem">
+        <span> (${mensagens[i].time}) ${mensagens[i].from} para ${mensagens[i].to}: ${mensagens[i].text} </span> 
+        </div>
+        `
+        chat.scrollIntoView(false);
+
+        }
+        if(mensagens[i].type==="private_message"){
+            chat.innerHTML += `
+        <li class="li.reservada">
+        <div class="caixamensagem">
+        <span> (${mensagens[i].time}) ${mensagens[i].from} reservadamente para ${mensagens[i].to}: ${mensagens[i].text} </span> 
+        </div>
+        `
+        chat.scrollIntoView(false);
+
+        }
+
+        
+    }
+    console.log(mensagens);
+   }
+
+
 
 function enviar(){
 
