48b07e9284a079cc82c15634592e61249f35e500
gabriel <gmoraes.mat@gmail.com>
2022-04-18 13:19:28 -0300

    Sexta versão, espaçamento de li

--- java.js
+++ java.js
@@ -3,12 +3,12 @@ let nome = "";
 let teste =[];
 let variavellocal = [];
 let nomeusuario = "";
+let statusCode = 0;
 
 
 
 function inicio(){
-    //alert("inicio funcioanndo");
-    
+        
     nomeusuario = prompt("Qual o seu nome ?") ;
         
     nome = {name: nomeusuario};    
@@ -19,47 +19,42 @@ function inicio(){
 inicio();
 
 function tratarErro(){ 
-  
-    console.log("deu ruim");
     alert("Por favor, entre com outro nome de usuario !");
     inicio();   
  }
 
  function tratarSucesso(){
-    //console.log("deu bom"); 
-   // alert("Bom bapo ;)");
+
     variavellocal = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages"); 
     variavellocal.then(carregarpagina);     
  }
 
  function confirmacaoStatus(){
     requisicao = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', nome); 
-
-    requisicao.catch(usuarioDeslogado)
-   
+    requisicao.then(tratarStatus);  
+}
+function tratarStatus(resposta){
+        statusCode = resposta.status;
+        console.log(statusCode);
 }
  const meuInterval = setInterval(confirmacaoStatus, 5000);
  const carregaInterval = setInterval(tratarSucesso, 3000);
  
- function usuarioDeslogado(){
-    window.location.reload();
- }
+
 
 function carregarpagina(mensagens){
     mensagens = mensagens.data;
     let chat = document.querySelector(".meio");
-    //chat.scrollIntoView(true);
     chat.innerHTML = "";
     
-
     for(i=0; i<mensagens.length; i++){
 
         if(mensagens[i].type==="status"){
             chat.innerHTML += `
         <li class="entrada">
         <div class="caixamensagem">
-        <span> (${mensagens[i].time}) ${mensagens[i].from} ${mensagens[i].text} </span> 
-        </div>
+        <span> (${mensagens[i].time})  ${mensagens[i].from} ${mensagens[i].text} </span> 
+        </div> 
         `
         chat.scrollIntoView(false);
 
@@ -68,7 +63,7 @@ function carregarpagina(mensagens){
             chat.innerHTML += `
         <li class="status">
         <div class="caixamensagem">
-        <span> (${mensagens[i].time}) ${mensagens[i].from} para ${mensagens[i].to}: ${mensagens[i].text} </span> 
+        <span> (${mensagens[i].time}) <b>${mensagens[i].from}</b> para ${mensagens[i].to}: ${mensagens[i].text} </span> 
         </div>
         `
         chat.scrollIntoView(false);
@@ -78,47 +73,34 @@ function carregarpagina(mensagens){
             chat.innerHTML += `
         <li class="reservada">
         <div class="caixamensagem">
-        <span> (${mensagens[i].time}) ${mensagens[i].from} reservadamente para ${mensagens[i].to}: ${mensagens[i].text} </span> 
+        <span> (${mensagens[i].time}) <b>${mensagens[i].from}</b> reservadamente para ${mensagens[i].to}: ${mensagens[i].text} </span> 
         </div>
         `
         chat.scrollIntoView(false);
-
-        }
-
-        
+        }        
     }
-    console.log(mensagens);
+    
    }
-
-
 
 function enviar(){
 
+    if(statusCode===200){
+
     let valorIput = document.querySelector("input").value;
-    console.log("Entrei e peguei o input");
-    
+        
     const objeto = {
         from: nomeusuario,
         to: "Todos",
         text: valorIput,
-        type: "message" // ou "private_message" para o bônus
+        type: "message" 
     };
-
-    console.log("Botei no objeto");
-    console.log(nomeusuario);
-    console.log(valorIput);
 
    let promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', objeto);
    promise.then(deubom);
    promise.catch(deuRuim);  
-}   
+ }   else window.location.reload();
+} 
 
-function deubom(){
-    console.log("Deu bom mano");
-}
 
-function deuRuim(){
-    console.log("Não foi dessa vez");
-}
 
 
